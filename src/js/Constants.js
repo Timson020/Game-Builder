a4a0524c564602f54fc69a3f88fa5f39cad7ebef
Timson020 <a_sa_f@126.com>
2018-07-13 00:02:51 +0800

    modify

---
+++ src/js/Constants.js
@@ -0,0 +1,11 @@
+const constants = {
+
+	// RESTFUL-API
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT',
+	PATH: 'PATH',
+	DEL: 'DELETE',
+}
+
+export default constants
---
+++ src/js/Services.js
@@ -0,0 +1,9 @@
+const host = 'xxx.com'
+
+const infix = '/game/xxx/'
+
+const services = {
+	request: `${host}${infix}`,
+}
+
+export default services
